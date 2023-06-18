f0b754a86963f6a719e7436af301aa83b969feab
rex992wang <wzx874946708@gmail.com>
2023-06-17 19:47:41 -0500

    initial the project, first commit

---
+++ weride/src/main/java/com/weride/weride/WerideApplication.java
@@ -0,0 +1,13 @@
+package com.weride.weride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerideApplication.class, args);
+	}
+
+}
---
+++ weride/src/test/java/com/weride/weride/WerideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weride.weride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
